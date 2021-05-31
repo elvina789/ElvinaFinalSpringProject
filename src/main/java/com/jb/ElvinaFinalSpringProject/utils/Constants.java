a047db7de9dde7eab327054d3fd89f89a47197f1
elvina <elvina789@gmail.com>
2021-05-31 22:59:30 +0300

    Adding local host

--- src/main/java/com/jb/ElvinaFinalSpringProject/utils/Constants.java
+++ src/main/java/com/jb/ElvinaFinalSpringProject/utils/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String ADMIN_EMAIL = "admin@admin.com";
     public static final String ADMIN_PASSWORD = "admin";
     public static final int ADMIN_USER_ID = -1;
+    public static final String LOCALHOST = "localhost";
 }
